8ccafffa4881e8fefc1ab3132bad824536c71dcc
Josh Edmondson <jedmondson@chapman.edu>
2020-11-14 12:15:14 -0800

    Updates username.sh

--- username.sh
+++ username.sh
@@ -6,7 +6,7 @@ echo "Use lower case characters, digits and underscore"
 echo " It must start with a lower case character"
 echo " It must contain at least 3 but not more than 12 characters"
 read USERNAME
-while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+while echo "$USERNAME" | egrep -v "^[a-z][a-z0-9_]{2,10}$" > /dev/null 2>&1
 do
 	echo "You must enter a valid username!"
 	echo "Enter a username: "
